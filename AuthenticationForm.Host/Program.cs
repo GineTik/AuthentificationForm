2cc71df248a4094995affd48e92e6ca21ad4f35b
GineTik <densheva18072003@gmail.com>
2022-12-29 00:52:47 +0200

    added swagger and several projects

--- AuthenticationForm.Host/Program.cs
+++ AuthenticationForm.Host/Program.cs
@@ -5,9 +5,23 @@ namespace AuthenticationForm.Host
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build(); 
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.MapGet("/hello_world", () => "Hello World!");
 
             app.Run();
         }
